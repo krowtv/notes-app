410aefd49ab3a3d35649564a553f8aac92f788a7
abe <justanotherartist365@gmail.com>
2024-05-05 09:35:35 -0400

    Created Notes App and added the following functionality: Active Notes bumps to the top of the Notes Sidebar. Notes summary title is first line of active Note. Notes sync with localStorage. Notes can now be deleted.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import 'react-mde/lib/styles/css/react-mde-all.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [notes, setNotes] = React.useState(
+      () => JSON.parse(localStorage.getItem("notes")) || []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (notes[0] && notes[0].id) || ""
+    )
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+
+    React.useEffect(() => {
+      if (notes.length > 0) {
+        localStorage.setItem("notes", JSON.stringify(notes));
+      }
+    }, [notes]);
+
+    function updateNote(text) {
+        setNotes(oldNotes => {
+          const newArr = [];
+          oldNotes.map((oldNote) => {
+            if (oldNote.id === currentNoteId) {
+              oldNote = {...oldNote, body: text};
+              newArr.unshift(oldNote);
+            }
+            else
+              newArr.push(oldNote);
+          })
+          return newArr;
+        })
+      }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+
+    function deleteNote(event, noteId) {
+      event.stopPropagation();
+      setNotes(oldNotes => oldNotes.filter((note) => note.id !== noteId));
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
 }
-
-export default App
